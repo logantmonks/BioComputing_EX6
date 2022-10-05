87198d9fadb8488f81e769f482619c3d1f2391cf
Peter Martin <pmarti23@nd.edu>
2022-10-04 23:20:42 -0400

    Wrote and tested code for Step 1

--- Files/MARTIN_PETER_Exercise6.sh
+++ Files/MARTIN_PETER_Exercise6.sh
@@ -1,3 +1,5 @@
 # A shell script that takes the input wages.csv and accomplishes the three tasks
 # outlined in the assignment
 # Usage: bash MARTIN_PETER_Exercise6.sh wages.csv
+
+tail -n +2 "$1" | cut -d, -f 1,2 | tr ', ' ' ' | sort -t ' ' -k1,1 -k2,2nr > gender_YEsort.txt
