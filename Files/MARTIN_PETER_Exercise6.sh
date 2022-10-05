f32601199f3bd2fa1c3d17caa5667c9a29747651
Peter Martin <pmarti23@nd.edu>
2022-10-05 00:01:12 -0400

    Wrote and tested code for the second step

--- Files/MARTIN_PETER_Exercise6.sh
+++ Files/MARTIN_PETER_Exercise6.sh
@@ -3,3 +3,14 @@
 # Usage: bash MARTIN_PETER_Exercise6.sh wages.csv
 
 tail -n +2 "$1" | cut -d, -f 1,2 | tr ', ' ' ' | sort -t ' ' -k1,1 -k2,2nr > gender_YEsort.txt
+
+tail -n +2 wages.csv | cut -d, -f 1,2,4 | sort -n -r -t, -k 3 > earnings_sort.txt
+echo ' '
+echo "Highest earner's gender, yearsExperience, and wage:"
+head -n 1 earnings_sort.txt
+echo ' '
+echo "Lowest earner's gender, yearsExperience, and wage:" 
+tail -n 1 earnings_sort.txt
+echo ' '
+echo "The number of females in the top ten earners of this data set:"
+head -n 10 earnings_sort.txt | grep -w -c 'female'
