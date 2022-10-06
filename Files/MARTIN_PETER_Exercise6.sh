de0ae47ba92d3dcdc9fc4ecfe27d968ef46a57c3
Peter Martin <pmarti23@nd.edu>
2022-10-06 18:46:59 -0400

    Edited the code for step 1 and wrote the code for step 3

--- Files/MARTIN_PETER_Exercise6.sh
+++ Files/MARTIN_PETER_Exercise6.sh
@@ -2,7 +2,7 @@
 # outlined in the assignment
 # Usage: bash MARTIN_PETER_Exercise6.sh wages.csv
 
-tail -n +2 "$1" | cut -d, -f 1,2 | tr ', ' ' ' | sort -t ' ' -k1,1 -k2,2nr > gender_YEsort.txt
+tail -n +2 "$1" | cut -d, -f 1,2 | tr ', ' ' ' | sort -t ' ' -k1,1 -k2,2nr | uniq > gender_YEsort.txt
 
 tail -n +2 wages.csv | cut -d, -f 1,2,4 | sort -n -r -t, -k 3 > earnings_sort.txt
 echo ' '
@@ -14,3 +14,9 @@ tail -n 1 earnings_sort.txt
 echo ' '
 echo "The number of females in the top ten earners of this data set:"
 head -n 10 earnings_sort.txt | grep -w -c 'female'
+
+highschool=$(tail -n +2 wages.csv | cut -d, -f 3,4 | tr ', ' ' ' | grep -w -E '12 [1-9]+\.[1-9]{1,}' | sort -t ' ' -k1,1nr -k2,2n | head -n 1 | cut -d ' ' -f 2)
+college=$(tail -n +2 wages.csv | cut -d, -f 3,4 | tr ', ' ' ' | grep -w -E '16 [1-9]+\.[1-9]{1,}' | sort -t ' ' -k1,1nr -k2,2n | head -n 1 | cut -d ' ' -f 2)
+echo "The minimum wage of people who graduate college is"
+echo "$college - $highschool" | bc
+echo "more dollars than the minimum wage of people who only graduate from high school"
