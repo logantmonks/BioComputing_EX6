6ab27c63322011488371ca19b959a1780f8b669e
Sarah Chong <schong3@nd.edu>
2022-10-07 09:25:55 -0500

    Modified- bc now works

--- Files/ex6.sh
+++ Files/ex6.sh
@@ -14,8 +14,8 @@ echo number of females in the top ten earners
 cat wages.csv | sort -n -t , -k 4 | tail -n 10 | grep -c female
 
 #Comparing difference in minimum wage between 12 yrs and 16 years of school
-12yearsschool=$(cat wages.csv | cut -d , -f 3,4 |tr "," " " | grep -w 12 |sort -n -k 2| head -n 1 |cut -d " " -f 2) 
-16yearsschool=$(cat wages.csv | cut -d , -f 3,4 |tr "," " " | grep -w 16 |sort -n -k 2| head -n 1 |cut -d " " -f 2)
-echo "$16yearsschool - $12yearsschool" | bc .
+highschool=$(cat wages.csv | cut -d , -f 3,4 |tr "," " " | grep -w 12 |sort -n -k 2| head -n 1 |cut -d " " -f 2) 
+collegeschool=$(cat wages.csv | cut -d , -f 3,4 |tr "," " " | grep -w 16 |sort -n -k 2| head -n 1 |cut -d " " -f 2)
+echo "$collegeschool - $highschool" | bc
 
 
